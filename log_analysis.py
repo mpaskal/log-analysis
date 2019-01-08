7a4dfae522ee663d3cc2fb30eb2f775bfff8d0d9
Marina Paskal <paskalm@rogers.com>
2019-01-08 00:19:22 -0500

    Refactoring

--- log_analysis.py
+++ log_analysis.py
@@ -5,77 +5,77 @@ import psycopg2
 dbname = "news"
 
 
-#def before_query():
-#    db = psycopg2.connect(database=dbname)
-#    c = db.cursor()
-#    return db, c
-
-
-#def after_query(query, db, cursor):
-#    cursor.execute(query)
-#    result = cursor.fetchall()
-#    db.close()
-#    return result
-	
-def after_query(query):
+def run_query(query):
     db = psycopg2.connect(database=dbname)
     c = db.cursor()
     c.execute(query)
-    result = cursor.fetchall()
+    result = c.fetchall()
     db.close()
     return result
 
 
-# The most popular three articles of all time
 def article():
-    # Print titles and number of views of the articles
-   # db, c = before_query()
-    query = "SELECT title, COUNT(title) AS views \
-    FROM articles, log \
-    WHERE log.path = concat( '/article/' , articles.slug ) \
-    GROUP BY title ORDER BY views DESC LIMIT 3"
-    q_result = after_query(query)
+    """
+    The most popular three articles of all time.
+    Print titles and number of the articles.
+    """
+    query = """
+            SELECT title, COUNT(title) AS views \
+            FROM articles, log \
+            WHERE log.path = concat( '/article/' , articles.slug ) \
+            GROUP BY title ORDER BY views DESC LIMIT 3;
+            """
+    q_result = run_query(query)
     print("\nThe most popular three articles of all time:\n")
-    print("Article                            " + "|" + " # of views   ")
-    for i in range(0, len(q_result), 1):
-        print("\"" + str(q_result[i][0]) + "\" | " + str(q_result[i][1]))
+    print('{:<40}|{:>20}'.format("Article", "# of views"))
+    print('-' * 61)
+    for article, views in q_result:
+        print('{:<40}|{:>20}'.format(article, views))
 
 
-# The most popular article authors of all time
 def authors():
-    # Print authors and number of views
- #   db, c = before_query()
-    query = "SELECT * FROM \
-    (SELECT authors.name, COUNT(articles.author) AS views \
-    FROM authors, articles, log \
-    WHERE authors.id = articles.author \
-    and log.path = concat( '/article/' , articles.slug ) \
-    GROUP BY authors.name ORDER BY views DESC) AS authors_subq"
-    q_result = after_query(query, db, c)
+    """
+    The most popular article authors of all time.
+    Print authors and number of views.
+    """
+    query = """
+            SELECT authors.name, COUNT(articles.author) AS views \
+            FROM authors, articles, log \
+            WHERE authors.id = articles.author \
+            and log.path = concat( '/article/' , articles.slug ) \
+            GROUP BY authors.name ORDER BY views DESC
+            """
+    q_result = run_query(query)
     print("\n\nThe most popular article authors of all time:\n")
-    print("Author's name   " + "|" + " # of views  ")
-    for i in range(0, len(q_result), 1):
-        print(q_result[i][0] + " | " + str(q_result[i][1]))
+    print('{:<40}|{:>20}'.format("Author's name", "# of views"))
+    print('-' * 61)
+    for authors, views in q_result:
+        print('{:<40}|{:>20}'.format(authors, views))
 
 
-# Days with more than 1% of requests lead to errors
 def log_errors():
-    # Print days with more than 1% of requests lead to errors
- #   db, c = before_query()
-    query = "SELECT * FROM \
-    (SELECT TO_CHAR(date, 'Mon DD, YYYY'), Error, Total, \
-    ROUND((Error * 100.0)/Total, 2) \
-    AS Percent \
-    FROM (SELECT time::timestamp::date AS Date, COUNT(status) AS Total, \
-    SUM(case WHEN status = '404 NOT FOUND' THEN 1 else 0 end) AS Error \
-    FROM log GROUP BY time::timestamp::date) AS result \
-    WHERE (Error * 100.0)/Total > 1.0 \
-    ORDER BY Percent DESC) AS errors_subq"
-    q_result = after_query(query, db, c)
+    """
+    Days with more than 1% of requests lead to errors
+    Print days with more than 1% of requests lead to errors
+    """
+    query = """
+            SELECT TO_CHAR(date, 'Mon DD, YYYY') AS Day, Error,
+            Total, ROUND((Error * 100.0)/Total, 2) \
+            AS Percent \
+            FROM (SELECT time::timestamp::date AS Date, \
+            COUNT(status) AS Total, \
+            SUM(case WHEN status = '404 NOT FOUND' THEN 1 else 0 end) \
+            AS Error \
+            FROM log GROUP BY time::timestamp::date) AS result \
+            WHERE (Error * 100.0)/Total > 1.0 \
+            ORDER BY Percent DESC
+            """
+    q_result = run_query(query)
     print("\n\nDays with more than 1% of requests lead to errors:\n")
-    print("Date         " + "|" + " % of errors  ")
-    for i in range(0, len(q_result), 1):
-        print(q_result[i][0] + " | " + str(q_result[i][3]))
+    print('{:<40}|{:>20}'.format("Date", "# of errors"))
+    print('-' * 61)
+    for day, error, total, percent in q_result:
+        print('{:<40}|{:>20}'.format(day, percent))
 
 
 if __name__ == '__main__':
